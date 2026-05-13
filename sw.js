17f4d6b34251301f5f5ef046e65c663e2a5ef896
jalopyossan <jalopy801@gmail.com>
2026-05-13 18:37:17 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,3 @@
-const CACHE_NAME='fps-tool-premium-pwa-tablet-v1';
-const ASSETS=['./','./index.html','./manifest.json','./sw.js','./icon.png','./splash.png'];
-self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null))).then(() => self.clients.claim()));
-});
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    caches.match(e.request).then(cached => {
-      if (cached) return cached;
-      return fetch(e.request).then(res => {
-        const copy = res.clone();
-        caches.open(CACHE_NAME).then(c => c.put(e.request, copy));
-        return res;
-      }).catch(() => caches.match('./index.html'));
-    })
-  );
-});
+const CACHE='pubg-clean-rebuild-v1';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.png']))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
